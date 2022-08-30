39a594552a26eddccd70c84e045522e7993dc2f2
Tu Nombre <you@example.com>
2022-08-30 16:00:22 +0200

    register page

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  get emailErrorMsg():string{
+    const errors=this.myForm.get('email')?.errors;
+    if(errors?.['required']){
+      return 'Email obligatorio'
+    }
+
+    if(errors?.['pattern']){
+      return 'El email no es válido'
+    }
+    if(errors?.['emailTomado']){
+      return 'Este email ya está en uso'
+    }
+
+
+    return '';
+  }
+
+  myForm:FormGroup=this.fb.group({
+    email:[''],
+    username:[''],
+    password:[''],
+    password2:['']
+  });
+
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  submitForm(){
+
+  }
+
+  checkFieldErrors(field:string){
+
   }
 
 }
